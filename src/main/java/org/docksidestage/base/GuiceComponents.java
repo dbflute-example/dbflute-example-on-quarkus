a47cbfc289b181f00c61994c6f673face3458b90
miyasama <takeshi.kato@creativegear.co.jp>
2024-06-30 16:17:20 +0900

    Set up to be able to DI with Google Guice

---
+++ src/main/java/org/docksidestage/base/GuiceComponents.java
@@ -0,0 +1,16 @@
+package org.docksidestage.base;
+
+import com.google.inject.Injector;
+
+public class GuiceComponents {
+
+    protected static Injector injector;
+
+    public static void acceptInjector(Injector initializedInjector) {
+        injector = initializedInjector;
+    }
+
+    public static <COMPONENT> COMPONENT find(Class<COMPONENT> type) {
+        return injector.getInstance(type);
+    }
+}
